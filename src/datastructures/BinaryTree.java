99292629b27f39bb9f4be7e586f14449ab16b55b
Kshitij verma <kvermaaii@gmail.com>
2024-12-26 17:37:45 +0530

    add largestPath function

--- src/datastructures/BinaryTree.java
+++ src/datastructures/BinaryTree.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import java.util.ArrayList;
+
 public class BinaryTree {
 	
 	Node root;
@@ -184,17 +186,58 @@ public class BinaryTree {
 		System.out.println(travel.value);		
 	}
 	
-    public void display(Node node) {
+    public void inorder(Node node) {
         if (node == null) {
             return;
         }
-        display(node.left);
+        inorder(node.left);
 
         System.out.print(node.value + " ");
 
-        display(node.right);
+        inorder(node.right);
     }
-	
+    
+    public void preorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preorder(node.left);
+        preorder(node.right);
+        
+    }
+    
+    public void postorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value + " ");
+    }
+    
+    public ArrayList<Integer> levelorder(Node root) {
+    	Queue<Node> queue = new Queue<>();
+    	ArrayList<Integer> list = new ArrayList<>();
+    	queue.enqueue(root);
+    	while(!queue.isEmpty()) {
+    		Node curr = queue.dequeue();
+    		System.out.print(curr.value + " ");
+    		list.add(curr.value);
+    		if(curr.left != null) {
+    			queue.enqueue(curr.left);
+    		}
+    		if(curr.right != null) {
+    			queue.enqueue(curr.right);
+    		}
+    		}
+    	return list;
+    }
+    
+    public void mirror() {
+    	
+    }
+    
 	public static class  Node{
 		int value;
 		Node right;
@@ -204,4 +247,103 @@ public class BinaryTree {
 			this.value = value;
 		}
 	}
+	
+	
+	public int largestPath() {
+		if(this.root == null) {
+			return 0;
+		}
+		//find the max path in left of root
+		//find the max path in right of root
+//		Node right = this.root.right; 
+//		Node left = this.root.left;
+//		int[] rightCount = {0};
+//		int[] leftCount = {0};
+//		count = 0;
+//		calculateLargestPathLength(left, leftCount);
+		return 1 + 
+				calculateLargestPathLength(this.root.left, 0) + 
+				calculateLargestPathLength(this.root.right, 0);
+	}
+
+    
+    public int calculateLargestPathLength(Node node, int count) {
+        if (node == null) {
+            return count;
+        }
+        return Math.max(
+        	calculateLargestPathLength(node.left,1+count), 
+        	calculateLargestPathLength(node.right,1+count)
+        );
+    }
+    
+    public int calculateLargestPathLengthAlt(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1+Math.max(
+        	calculateLargestPathLengthAlt(node.left), 
+        	calculateLargestPathLengthAlt(node.right)
+        );
+    }
+    
 }
+
+ class MirrorTree extends BinaryTree{
+	 
+	@Override
+	public void insert(int value) {
+		if(this.root == null) 
+		{
+			Node newRoot = new Node(value);
+			this.root = newRoot;
+		} else {
+			Node newChild = new Node(value);
+			Node travel = this.root;
+			Node parent = travel;
+			while(travel != null) {
+				parent = travel;
+				if(travel.value < value)
+				{
+					travel = travel.left;
+				} else {
+					travel = travel.right;
+				}
+			}
+		    if(parent.value < value) 
+		    {
+		    	parent.left = newChild;
+			} else {
+				parent.right = newChild;
+			}
+		}
+	}
+}
+
+
+/*
+       3 
+    2     5
+   1       6
+ 
+ 3  print 3
+ node.left 2
+ node.right 5    2 5    
+ node.left.left 1
+ node.left.righ 0
+ node.right.left 4
+ node.right.right 6
+ 
+ 2 5 1 0 4 6 
+ 
+        15                   15
+    6        18         18         6
+  3   7     17 20     20  17     7   3
+ 2 4   13                      13   2  4
+      9                          9
+ */
+
+
+
+
+
